0cdd6723016c1a273a1563117d7b48f64ceaa263
Taras <81424893+tar1kkk@users.noreply.github.com>
2022-07-15 16:53:09 +0300

    Redux and React. Redux saga asynchronous actions

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import style from './style.css'
+import { useDispatch, useSelector } from "react-redux";
+import { asyncDecrementCreator, asyncIncrementCreator, decrementCreator, incrementCreator } from "./store/countReducer";
+import { fetchUsers } from "./store/userReducer";
+
 
 function App() {
+  const count = useSelector(state => state.countReducer.count)
+  const users = useSelector(state => state.userReducer.users)
+  const dispatch = useDispatch()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="count">{count}</div>
+      <div className="btns">
+        <button className="btn" onClick={() => dispatch(asyncIncrementCreator())}>ИНКРЕМЕНТ++</button>
+        <button className="btn" onClick={() => dispatch(asyncDecrementCreator())}>ДЕКРЕМЕНТ--</button>
+        <button className="btn" onClick={() => dispatch(fetchUsers())}>ПОЛУЧИТЬ ЮЗЕРОВ--</button>
+      </div>
+      <div className="users">
+        {users.map(user =>
+          <div className="user">
+            {user.name}
+          </div>
+        )}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
